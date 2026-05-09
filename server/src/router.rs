8b2cdd19e44d03c8dfa75071507b87cafebc152e
xairaven <alexkovalevkpi@gmail.com>
2026-05-10 01:34:54 +0300

    (Server) Feat: Users & Broadcast routing

--- server/src/router.rs
+++ server/src/router.rs
@@ -56,36 +56,74 @@ impl Router {
                     println!("User disconnected: {}", user_id);
                 },
                 RouterEvent::RouteMessage {
-                    target, message, ..
+                    from,
+                    target,
+                    message,
                 } => {
-                    // Logic to find the recipient channel and send the message
                     match target {
-                        Target::User(target_user_id) => {
+                        Target::User(ref target_user_id) => {
                             if let Some(client_channel) =
-                                self.active_connections.get(&target_user_id)
+                                self.active_connections.get(target_user_id)
                             {
-                                // Ignore error if the client disconnected right before we send
-                                let _ = client_channel.send(message).await;
+                                // Online: Send immediately
+                                let _ = client_channel.send(message.clone()).await;
                             } else {
-                                // TODO: Here we will save it to Postgres offline queue later
+                                // Offline: Just print log, DB logic handles saving below
                                 println!(
-                                    "User {} is offline, message not delivered immediately",
-                                    target_user_id
+                                    "User {} is offline. Message will be queued.",
+                                    target_user_id.0
                                 );
                             }
+
+                            // DB INSERTS MUST NOT BLOCK THE ROUTER
+                            // Spawn a background task to save the message to Postgres
+                            self.save_message(&from, Some(target_user_id), &message);
                         },
                         Target::Broadcast => {
+                            // Send to everyone who is online
                             for channel in self.active_connections.values() {
                                 let _ = channel.send(message.clone()).await;
                             }
+                            self.save_message(&from, None, &message);
                         },
                         Target::Group(_) => {
-                            // TODO: Here we will query DB for members and route to their channels
+                            // TODO: Group routing logic and DB saving
                             println!("Group routing not yet implemented");
                         },
                     }
                 },
             }
         }
+    }
+
+    fn save_message(
+        &self, from: &UserId, target: Option<&UserId>, message: &ServerMessage,
+    ) {
+        // Save broadcast message to DB
+        let pool = self.pool.clone();
+        let msg_clone = message.clone();
+        let from_id = from.0;
+        let target_id = target.map(|user_id| user_id.0);
+        let is_broadcast = target_id.is_none();
+
+        tokio::spawn(async move {
+            if let ServerMessage::NewMessage {
+                content, timestamp, ..
+            } = msg_clone
+                && let Ok(payload_bytes) = postcard::to_stdvec(&content)
+            {
+                let _ = sqlx::query!(
+                        "INSERT INTO messages (sender_id, target_user_id, is_broadcast, content_payload, created_at) \
+                        VALUES ($1, $2, $3, $4, $5)",
+                        from_id,
+                        target_id,
+                        is_broadcast,
+                        payload_bytes,
+                        timestamp
+                    )
+                    .execute(&pool)
+                    .await;
+            }
+        });
     }
 }
